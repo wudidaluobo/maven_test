46d060be6417556da44d33946200c342117b5074
wudidaluobo <1483858420@qq.com>
2024-02-23 14:15:04 +0800

    Initial commit

---
+++ maven_java/src/main/java/pojo/User.java
@@ -0,0 +1,15 @@
+package pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    private Integer uid;
+    private String name;
+
+
+}
